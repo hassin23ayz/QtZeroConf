fd087a9327d583add047da28a465de79c709c345
ayz03 <hassin23ayz@gmail.com>
2018-03-30 17:38:18 +0600

    publish , browse tested

--- example/main.cpp
+++ example/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	app.setApplicationName("QtZeroConf Example");
+    app.setApplicationName("Bonjour ... QtZeroConf Example");
 	app.setApplicationVersion("0.1");
 
 	mainWindow window;
